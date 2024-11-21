fc82c3743c048a5b711d51e8b23c8c4cf9cdf926
Kendall <kendall.duran@optisoftla.com>
2024-11-21 15:36:45 -0600

    first commit

---
+++ src/orders/dto/status.dto.ts
@@ -0,0 +1,10 @@
+import { IsEnum } from "class-validator";
+import { OrderStatus, OrderStatusList } from "../enum/order.enum";
+
+export class StatusDto{
+    @IsEnum(OrderStatusList,{
+        message:`Valid status are ${OrderStatusList}`
+    })
+    status?:OrderStatus
+
+}
